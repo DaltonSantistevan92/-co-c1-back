d2582415f66ec135b625204d9cc1db92b2c64b38
sergio roger <sergio.lkg.31@gmail.com>
2022-01-30 23:37:49 -0500

    Methods for entries

---
+++ controllers/entradaController.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once 'app/cors.php';
+require_once 'app/request.php';
+require_once 'core/conexion.php';
+require_once './models/entradaModel.php';
+require_once './models/usuarioModel.php';
+
+class EntradaController {
+
+    private $limiteKey = 0;
+
+    public function __construct(){ 
+        $this->limiteKey = 6;
+    }
+
+    public function create(Request $request){
+
+        $this->cors->corsJson();
+        $entradaData = $request->input('entrada');
+        $entradaData->qr;
+
+        //Buscar el usuario
+        $user = Usuario::where('code_qr')->first();
+        $helper = new Helper();
+        $entrada = new Entrada();
+
+        $entrada->usuario_id = $user->id;
+        $entrada->clave = $helper->generate_key($this->limiteKey);
+        $entrada->hora = date('H:i:s');
+        $entrada->fecha = date('Y-m-d');
+        $entrada->save();
+
+        $response = [
+            'status' =>  true,
+            'message' => 'Entrada registrada de '.$user->usuario
+        ];
+
+        echo json_encode($response);
+    }
+
+    public function lastEntry($params){
+        $usuario_id = intval($params['usuario_id']);
+
+        $last = Entrada::where('usuario_id', $usuario_id)->orderBy('id', 'desc')->first();
+
+        if($last){
+            $response = [
+                'status' => true,
+                'message' => 'Existe un dato',
+                'data' => $last
+            ];
+        }else{
+            $response = [
+                'status' => false,
+                'message' => 'No existe datos',
+                'data' => false
+            ];
+        }
+
+        echo json_encode($response);
+    }
+}
