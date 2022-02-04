09d20909d2c3fa66942892d528bbc72cde4176a3
sergio roger <sergio.lkg.31@gmail.com>
2022-02-04 03:55:28 -0500

    valida consulta

--- controllers/rol_pagoController.php
+++ controllers/rol_pagoController.php
@@ -8,7 +8,8 @@ require_once 'app/request.php';
 require_once 'models/rol_pagoModel.php';
 
 
-class Rol_PagoController {
+class Rol_PagoController
+{
 
     private $cors;
     private $conexion;
@@ -22,9 +23,12 @@ class Rol_PagoController {
         $this->roleMechanic = 2;
     }
 
-    public function guardar(Request $request) { }
+    public function guardar(Request $request)
+    {
+    }
 
-    public function create_detail_pay($params){
+    public function create_detail_pay($params)
+    {
 
         $key = $params['clave'];
 
@@ -33,8 +37,8 @@ class Rol_PagoController {
         $sale = Salida::where('clave', $key)->first();
 
         //Make calc hour for sale minus entry
-        $timestapEntry = new DateTime($entry->fecha.' '.$entry->hora);  //Menor
-        $timestapSale = new DateTime($sale->fecha.' '.$sale->hora);     //Mayor
+        $timestapEntry = new DateTime($entry->fecha . ' ' . $entry->hora);  //Menor
+        $timestapSale = new DateTime($sale->fecha . ' ' . $sale->hora);     //Mayor
 
         $diff = $timestapSale->diff($timestapEntry);
         $hour =  $diff->format('%h');                   //Including hour eats
@@ -44,36 +48,34 @@ class Rol_PagoController {
         //Identify type role user and extrac money month
         $user = Usuario::find($entry->usuario_id);
         $salario = Salario::where('rol_id', $user->rol_id)->first();
-        
+
         $detail = null;
         //Verify hour most 8 and mechanic
-        if($user->rol_id == $this->roleMechanic){   
+        if ($user->rol_id == $this->roleMechanic) {
             // var_dump($hour);    die();
 
-            if($hour > $this->hourLaborable){  
+            if ($hour > $this->hourLaborable) {
 
                 $diff = ($hour - $this->hourLaborable);
 
                 //Normal
                 $newCal = round(floatval($this->hourLaborable) * floatval($salario->salario_hora), 2);
                 $detailNormal = $this->makeDetail($user->id, $this->hourLaborable, $salario->salario_hora, $newCal);
-                
+
                 //Extra plus percent fee
-                $newSaleHour = round(((floatval($salario->salario_hora) * $salario->porcentaje_comision)/100),2) + $salario->salario_hora;
+                $newSaleHour = round(((floatval($salario->salario_hora) * $salario->porcentaje_comision) / 100), 2) + $salario->salario_hora;
                 $total = round(floatval($diff) * floatval($newSaleHour), 2);
                 $detailExtra = $this->makeDetail($user->id, $diff, $newSaleHour, $total, 'E');
-
-            }else{
+            } else {
                 $total = round(floatval($hour) * floatval($salario->salario_hora), 2);
                 $detail = $this->makeDetail($user->id, $hour, $salario->salario_hora, $total);
             }
-
-        }else{  //Other role
+        } else {  //Other role
             //Make to data for detail_pay
             $total = round(floatval($hour) * floatval($salario->salario_hora), 2);
             $detail = $this->makeDetail($user->id, $hour, $salario->salario_hora, $total);
         }
-        
+
         $response = [
             'status' => true,
             'mensaje' => 'Detail hour generated !!'
@@ -81,8 +83,9 @@ class Rol_PagoController {
 
         echo json_encode($response);
     }
-    
-    private function makeDetail($user_id, $hour, $sale_hour, $total, $type = 'N'){
+
+    private function makeDetail($user_id, $hour, $sale_hour, $total, $type = 'N')
+    {
         $detailPay = new Detalles_Pagos();
 
         $detailPay->usuario_id = $user_id;
@@ -95,7 +98,8 @@ class Rol_PagoController {
         return $detailPay;
     }
 
-    public function createRolPago($params){
+    public function createRolPago($params)
+    {
 
         $inicio = $params['inicio'];
         $fin = $params['fin'];
@@ -103,67 +107,86 @@ class Rol_PagoController {
 
         //Consultar a los detalles pagos
         $detalleHorasNormal = Detalles_Pagos::where('usuario_id', $user_id)->where('type', 'N')
-            ->whereDate('created_at','>=', $inicio)->whereDate('created_at', '<=', $fin)->get();
+            ->whereDate('created_at', '>=', $inicio)->whereDate('created_at', '<=', $fin)->get();
         $detalleHorasExtras = Detalles_Pagos::where('usuario_id', $user_id)->where('type', 'E')
-            ->whereDate('created_at','>=', $inicio)->whereDate('created_at', '<=', $fin)->get();
+            ->whereDate('created_at', '>=', $inicio)->whereDate('created_at', '<=', $fin)->get();
         $detalleHorasTotales = Detalles_Pagos::where('usuario_id', $user_id)
-            ->whereDate('created_at','>=', $inicio)->whereDate('created_at', '<=', $fin)->get();
-          
+            ->whereDate('created_at', '>=', $inicio)->whereDate('created_at', '<=', $fin)->get();
+
         $response = [];
-        $hoursExtras = 0;   $horas_normal = 0;
-        $totalExtras = 0;   $totalNormal = 0;
-
-        foreach($detalleHorasTotales as $extra){
-
-            if($extra->type == 'E'){
-                $hoursExtras += $extra->cant_hora;
-                $totalExtras += $extra->total;
-            }
-
-            if($extra->type == 'N'){
-                $horas_normal += $extra->cant_hora;
-                $totalNormal += $extra->total;
-            }
-        }
+        $hoursExtras = 0;
+        $horas_normal = 0;
+        $totalExtras = 0;
+        $totalNormal = 0;
 
         $user = Usuario::find($user_id);
         $cargo = $user->rol->cargo;
         $porcentaje_iess = 5;
 
-        $total = $totalExtras + $totalNormal;
-        $aporteIess = round(floatval(($total * $porcentaje_iess)/100),2);
+        if ($detalleHorasTotales->count() > 0) {
+            foreach ($detalleHorasTotales as $extra) {
 
-        $sueldo = $total - $aporteIess;
+                if ($extra->type == 'E') {
+                    $hoursExtras += $extra->cant_hora;
+                    $totalExtras += $extra->total;
+                }
 
-        $response = [
-            'usuario_id' => $user_id,
-            'cargo' => $cargo,
-            'persona' => $user->persona,
-            'horas_normales' => $horas_normal,
-            'horas_extras' => $hoursExtras,
-            'total_normal' => $totalNormal,
-            'total_extra' => $totalExtras,
-            'porcentaje_iess' => $porcentaje_iess,
-            'aporte_iess' => $aporteIess,
-            'total_ingresos' => $total,
-            'total_descuentos' => 0,
-            'sueldo_recibir' => $sueldo
-        ];
+                if ($extra->type == 'N') {
+                    $horas_normal += $extra->cant_hora;
+                    $totalNormal += $extra->total;
+                }
+            }
 
-        //Guardar el rol_pago
-        $rolPago = new Rol_Pago();
-        $rolPago->usuario_id = $response['usuario_id'];
-        $rolPago->horas_normales = $response['horas_normales'];
-        $rolPago->horas_extras = $response['horas_extras'];
-        $rolPago->total_normal = $response['total_normal'];
-        $rolPago->total_extra = $response['total_extra'];
-        $rolPago->porcentaje_iess = $response['porcentaje_iess'];
-        $rolPago->aporte_iess = $response['aporte_iess'];
-        $rolPago->total_ingresos = $response['total_ingresos'];
-        $rolPago->total_descuentos = $response['total_descuentos'];
-        $rolPago->sueldo_recibir = $response['sueldo_recibir'];
+            $total = $totalExtras + $totalNormal;
+            $aporteIess = round(floatval(($total * $porcentaje_iess) / 100), 2);
 
-        $rolPago->save();
+            $sueldo = $total - $aporteIess;
+
+            $response = [
+                'usuario_id' => $user_id,
+                'cargo' => $cargo,
+                'persona' => $user->persona,
+                'horas_normales' => $horas_normal,
+                'horas_extras' => $hoursExtras,
+                'total_normal' => $totalNormal,
+                'total_extra' => $totalExtras,
+                'porcentaje_iess' => $porcentaje_iess,
+                'aporte_iess' => $aporteIess,
+                'total_ingresos' => $total,
+                'total_descuentos' => 0,
+                'sueldo_recibir' => $sueldo
+            ];
+
+            //Guardar el rol_pago
+            $rolPago = new Rol_Pago();
+            $rolPago->usuario_id = $response['usuario_id'];
+            $rolPago->horas_normales = $response['horas_normales'];
+            $rolPago->horas_extras = $response['horas_extras'];
+            $rolPago->total_normal = $response['total_normal'];
+            $rolPago->total_extra = $response['total_extra'];
+            $rolPago->porcentaje_iess = $response['porcentaje_iess'];
+            $rolPago->aporte_iess = $response['aporte_iess'];
+            $rolPago->total_ingresos = $response['total_ingresos'];
+            $rolPago->total_descuentos = $response['total_descuentos'];
+            $rolPago->sueldo_recibir = $response['sueldo_recibir'];
+
+            $rolPago->save();
+        } else {
+            $response = [
+                'usuario_id' => $user_id,
+                'cargo' => $cargo,
+                'persona' => $user->persona,
+                'horas_normales' => $horas_normal,
+                'horas_extras' => $hoursExtras,
+                'total_normal' => $totalNormal,
+                'total_extra' => $totalExtras,
+                'porcentaje_iess' => $porcentaje_iess,
+                'aporte_iess' => 0,
+                'total_ingresos' => 0,
+                'total_descuentos' => 0,
+                'sueldo_recibir' => 0
+            ];
+        }
 
         echo json_encode($response);
     }
